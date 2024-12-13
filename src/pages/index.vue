0ad6d6547f0110a751cd0b0d2fe6e308c246116e
yuzhongxuan123 <yunzhongxi@outlook.com>
2024-12-13 11:14:52 +0800

    邮箱还原

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,8 +9,8 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-transparent p-2 p-x-4 text-5 style="color: #22C9E3;">
-        Dhurimakhadilkar@icloud.com
+      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+        639293382@qq.com
       </div>
     </div>
   </div>
