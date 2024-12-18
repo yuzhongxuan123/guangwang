4c94aa776d3ac253d3d6f09ba5c4192d9051aff7
yuzhongxuan123 <yunzhongxi@outlook.com>
2024-12-18 19:03:12 +0800

    修改

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,10 +6,10 @@ defineOptions({
 
 <template>
   <div relative min-h-100vh>
-    <img h-full w-full src="../assets/bg.webp" alt="">
+    <img h-full w-full src="../assets/bg.png" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
+      <div rd-2 bg-black p-2 p-x-5 text-8 color-purple>
         639293382@qq.com
       </div>
     </div>
