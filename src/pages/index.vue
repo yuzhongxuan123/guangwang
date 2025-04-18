d669739af868fb74ffcfa986c46424b0cc257c8d
yuzhongxuan123 <yunzhongxi@outlook.com>
2025-04-18 09:45:28 +0800

    1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,7 +10,7 @@ defineOptions({
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
       <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        Whq5961893@icloud.com
+        xxxlilnin@icloud.com
       </div>
     </div>
   </div>
